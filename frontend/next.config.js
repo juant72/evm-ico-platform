62a109b8f054aea426f67ec52230a1247952ce6b
Juan Tapia <juant72@gmail.com>
2025-03-12 06:00:18 +0000

    feat: update .gitignore, enhance ThirdwebProvider configuration, and add polyfills in webpack

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,10 +7,35 @@ const nextConfig = {
     "@thirdweb-dev/chains",
     "react-chartjs-2",
     "chart.js",
+    "@coinbase/wallet-sdk",
   ],
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      tls: false,
+      os: require.resolve("os-browserify/browser"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      stream: require.resolve("stream-browserify"),
+      crypto: require.resolve("crypto-browserify"),
+      buffer: require.resolve("buffer"),
+      process: require.resolve("process/browser"),
+    };
+
+    // Add polyfills
+    config.plugins.push(
+      new (require("webpack").ProvidePlugin)({
+        process: "process/browser",
+        Buffer: ["buffer", "Buffer"],
+      })
+    );
+
     return config;
+  },
+  // Ayuda con problemas de importación de módulos
+  experimental: {
+    esmExternals: "loose",
   },
 };
 
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,13 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Sepolia } from "@thirdweb-dev/chains";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // This can be any supported chain
-  const desiredChainId = ChainId.Mainnet;
-
   return (
-    <ThirdwebProvider desiredChainId={desiredChainId}>
+    <ThirdwebProvider activeChain={Sepolia}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
